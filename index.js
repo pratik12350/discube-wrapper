bab3467a38ad8ceb803022c62c8ed2a27a75b35c
/Pratik <67191233+pratik12350@users.noreply.github.com>
2021-11-16 10:20:46 +0000

    ???

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ module.exports.meme = async function meme() {
   return meme;
 }
 
+
 //SCREENSHOT 
 
 module.exports.screenshot = async function screenshot(url) {
   if(!url) throw new TypeError("Url Is Not Provided!")
   let ss = `${baseURL}/screenshot?url=${encodeURIComponent(url)}`
   return ss;
+}
+
+//YOUTUBE VIDEO FINDER
+
+module.exports.videoFinder = async function ytVideoFinderEndpoint(query) {
+  if(!query) throw new Error("Query Not Provided!")
+  let jsonFetch = await fetch(`https://discube.gq/youtube?name=${encodeURIComponent(query)}`)
+  let jsonData = await jsonFetch.json();
+
+  return jsonData.video_url;
 }
