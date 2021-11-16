fb5843e76b04236842b3236004b2e1ad17888818
/Pratik <67191233+pratik12350@users.noreply.github.com>
2021-11-16 07:43:34 +0000

    Added Meme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # discube-wrapper
 Discube Wrapper npm
+# under development!
---
+++ index.js
@@ -0,0 +1,9 @@
+const fetch = require("node-fetch");
+const baseURL = "https://discube.gq"
+
+//MEME
+
+module.exports = async function meme() {
+  let meme = `${baseURL}/meme`;
+  return meme;
+}
